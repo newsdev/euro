9f6392aa2edae01b23db3d6c270331be960ad15d
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-06-03 15:23:24 -0400

    Adds updates

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ app.debug=True
 
 VALID_HEADERS = ['x-meta-feed-type','x-meta-feed-parameters','x-meta-default-filename','x-meta-game-id','x-meta-competiiton-id','x-meta-season-id','x-meta-game-id','x-meta-gamesystem-id','x-meta-matchday','x-meta-game-status','x-meta-language','x-meta-production-server','x-meta-production-server-timeStamp','x-meta-production-server-module','x-meta-mime-type','x-meta-encoding','x-meta-sport-id','x-content-length','x-meta-id','x-feed_id','x-meta-date-created','x-meta-message-digest']
 
-@app.route('/<tournament>/<year>/create/', methods=['POST',])
+@app.route('/<tournament>/<year>/create/', methods=['POST', 'GET'])
 def parse_xml(tournament, year):
+    if request.method == 'GET':
+        return "Listening"
+
     if request.method == 'POST':
 
         timestamp = int(time.mktime(datetime.datetime.now().timetuple()))
