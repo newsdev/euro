f5f038233e7f61538751125dfd03e4c89a565d3b
Jeremy Bowers <jeremyjbowers@gmail.com>
2016-06-03 16:39:01 -0400

    Routes for Gregor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import glob
 import json
 import os
 import time
@@ -12,6 +13,24 @@ app = Flask(__name__)
 app.debug=True
 
 VALID_HEADERS = ['x-meta-feed-type','x-meta-feed-parameters','x-meta-default-filename','x-meta-game-id','x-meta-competiiton-id','x-meta-season-id','x-meta-game-id','x-meta-gamesystem-id','x-meta-matchday','x-meta-game-status','x-meta-language','x-meta-production-server','x-meta-production-server-timeStamp','x-meta-production-server-module','x-meta-mime-type','x-meta-encoding','x-meta-sport-id','x-content-length','x-meta-id','x-feed_id','x-meta-date-created','x-meta-message-digest']
+
+@app.route('/<tournament>/<year>/latest.xml', methods=['GET'])
+def latest_xml(tournament, year):
+    xml_files = sorted(glob.glob('%s/*.xml' % utils.DATA_DIR), key=lambda x:x.split('-')[0])
+    if len(xml_files) > 0:
+        with open(xml_files[0], 'r') as readfile:
+            return Response(readfile.read(), mimetype="text/xml")
+    else:
+        return ""
+
+@app.route('/<tournament>/<year>/latest.json', methods=['GET'])
+def latest_json(tournament, year):
+    json_files = sorted(glob.glob('%s/*-processed.json' % utils.DATA_DIR), key=lambda x:x.split('-')[0])
+    if len(json_files) > 0:
+        with open(json_files[0], 'r') as readfile:
+            return Response(json.loads(readfile.read()), mimetype="application/json")
+    else:
+        return ""
 
 @app.route('/<tournament>/<year>/create/', methods=['POST', 'GET'])
 def parse_xml(tournament, year):
